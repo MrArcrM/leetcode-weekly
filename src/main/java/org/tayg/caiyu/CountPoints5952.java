82af0a7834a82000970f604ece150e0abec47901
Yucai <1546868987@qq.com>
2021-12-12 20:23:00 +0800

    Code refactoring

--- src/main/java/org/tayg/caiyu/CountPoints5952.java
+++ src/main/java/org/tayg/caiyu/CountPoints5952.java
@@ -13,24 +13,43 @@ public class CountPoints5952 {
      * @return
      */
     public int CountPoints(String rings){
+        //为每条杠创建一个 HashSet 
         int polesNum = 10;
-        HashSet<Character>[] poles = new HashSet[polesNum];
-        for(int i = 0; i < polesNum; i++){
-            poles[i] = new HashSet<>();
-        }
-        for(int i = 0; i < rings.length(); i += 2){
-            int index = Character.getNumericValue(rings.charAt(i+1));
-            poles[index].add(rings.charAt(i));
-        }
+        HashSet<Character>[] poles = getPoles(polesNum);
+
+        //遍历rings，更新杠上的颜色
+        updatePoles(rings, poles);
+        
+        //获取杠上有三种颜色的 条数
         int count = 0;
+        count = getCount(poles, count);
+        
+        return count;
+    }
+
+    private int getCount(HashSet<Character>[] poles, int count) {
         for(HashSet<Character> pole:poles){
             if(pole.size()==3){
                 count++;
             }
         }
-        
-        
-
         return count;
     }
+
+    private void updatePoles(String rings, HashSet<Character>[] poles) {
+        for(int i = 0; i < rings.length(); i += 2){
+            int index = Character.getNumericValue(rings.charAt(i+1));
+            poles[index].add(rings.charAt(i));
+        }
+    }
+
+    private HashSet<Character>[] getPoles(int polesNum) {
+        HashSet<Character>[] poles = new HashSet[polesNum];
+        for(int i = 0; i < polesNum; i++){
+            poles[i] = new HashSet<>();
+        }
+        return poles;
+    }
+
+
 }
