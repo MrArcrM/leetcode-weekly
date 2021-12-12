2bfca18afcf41a7df90839fcec63a86178648a9f
guoqu <304899507@qq.com>
2021-12-12 16:31:38 +0800

    init

---
+++ src/main/java/org/tayg/guoqu/CountPoints5952.java
@@ -0,0 +1,59 @@
+/* CopyRight (c) org.tayg 2021-2021. All rights reserved. */
+
+package org.tayg.guoqu;
+
+import java.util.HashSet;
+
+/**
+ * solution for countPoints.
+ * 
+ * @author guoqu
+ * @since 2021-12-12
+ */
+public class CountPoints5952 {
+    /**
+     * count how many poles which have all 'RGB' rings.
+     * 
+     * @param rings String, rings to count
+     * @return int, count num
+     */
+    public int countPoints(String rings) {
+        int poleNum = 10;
+        HashSet<Character>[] poles = initPoles(poleNum);
+
+        poles = updatePoles(poles, rings);
+
+        int count = countPoles(poles);
+
+        return count;
+    }
+
+    private HashSet<Character>[] updatePoles(HashSet<Character>[] poles, String rings) {
+        for (int i = 0; i < rings.length(); i += 2) {
+            Character color = rings.charAt(i);
+            Character idxChar = rings.charAt(i + 1);
+            int idx = Character.getNumericValue(idxChar); // Character -> int
+            poles[idx].add(color);
+        }
+
+        return poles;
+    }
+
+    private int countPoles(HashSet<Character>[] poles) {
+        int count = 0;
+        for (int i = 0; i < poles.length; i++) {
+            if (poles[i].size() == 3) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private HashSet<Character>[] initPoles(int poleNum) {
+        HashSet<Character>[] poles = new HashSet[poleNum];
+        for (int i = 0; i < poleNum; i++) {
+            poles[i] = new HashSet<>();
+        }
+        return poles;
+    }
+}
---
+++ src/test/java/org/tayg/guoqu/TestCountPoints5952.java
@@ -0,0 +1,28 @@
+package org.tayg.guoqu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCountPoints5952 {
+    CountPoints5952 solution = new CountPoints5952();
+
+    @Test
+    public void testCountPoints() {
+        String rings1 = "B0B6G0R6R0R6G9";
+        int trueResult1 = 1;
+        int res1 = solution.countPoints(rings1);
+        assertEquals(trueResult1, res1);
+
+        String rings2 = "B0R0G0R9R0B0G0";
+        int trueResult2 = 1;
+        int res2 = solution.countPoints(rings2);
+        assertEquals(trueResult2, res2);
+
+        String rings3 = "G4";
+        int trueResult3 = 0;
+        int res3 = solution.countPoints(rings3);
+        assertEquals(trueResult3, res3);
+    }
+
+}
