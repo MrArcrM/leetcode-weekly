f7dc818c3b6d690f0318c7dbf945cb4500d2ccea
guoqu <304899507@qq.com>
2022-01-02 15:21:18 +0800

    modify pa, pb

--- src/main/java/org/tayg/guoqu/week274/CheckString5967.java
+++ src/main/java/org/tayg/guoqu/week274/CheckString5967.java
@@ -7,22 +7,16 @@ package org.tayg.guoqu.week274;
 public class CheckString5967 {
     public boolean checkString(String s) {
         int n = s.length();
-        int pa = n;
-        int pb = -1;
+        int pa = 0;
+        int pb = 0;
         // 1. get last pa index
-        for (pa = n; pa >= 0; pa--) {
-            if (pa >= n) {
-                continue;
-            }
+        for (pa = n - 1; pa >= 0; pa--) {
             if (s.charAt(pa) == 'a') {
                 break;
             }
         }
         // 2. get first pb index
-        for (pb = -1; pb < n; pb++) {
-            if (pb < 0) {
-                continue;
-            }
+        for (pb = 0; pb < n; pb++) {
             if (s.charAt(pb) == 'b') {
                 break;
             }
