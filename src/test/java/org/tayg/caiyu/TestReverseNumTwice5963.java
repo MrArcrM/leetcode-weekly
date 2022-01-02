b34d081b8057e9450888f0cd6805425669f7b957
Yucai <1546868987@qq.com>
2022-01-02 10:58:36 +0800

    Change Boolean Type

--- src/test/java/org/tayg/caiyu/TestReverseNumTwice5963.java
+++ src/test/java/org/tayg/caiyu/TestReverseNumTwice5963.java
@@ -12,19 +12,19 @@ public class TestReverseNumTwice5963 {
         ReverseNumTwice5963 solution = new ReverseNumTwice5963();
         int num = 526;
         boolean res = solution.isSameAfterReversals(num);
-        Boolean trueResult = true;
+        boolean trueResult = true;
         assertEquals(res, trueResult);
 
         ReverseNumTwice5963 solution1 = new ReverseNumTwice5963();
         int num1 = 1800;
         boolean res1 = solution1.isSameAfterReversals(num1);
-        Boolean trueResult1 = false;
+        boolean trueResult1 = false;
         assertEquals(res1, trueResult1);
 
         ReverseNumTwice5963 solution2 = new ReverseNumTwice5963();
         int num2 = 0;
         boolean res2 = solution2.isSameAfterReversals(num2);
-        Boolean trueResult2 = true;
+        boolean trueResult2 = true;
         assertEquals(res2, trueResult2);
 
     }
